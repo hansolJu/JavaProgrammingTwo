95b0b9c8202b541c9e906fd8593b17c614a3e626
컴퓨터 <@DESKTOP-C01FUL7>
2017-10-30 11:00:57 +0900

    11-23개의 이미지 아이콘을 가진 버튼 만들기 11p

---
+++ JavaProgrammingTwo/src/Chapter11/ButtonImageEx.java
@@ -0,0 +1,31 @@
+package Chapter11;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class ButtonImageEx extends JFrame {
+	Container contentPane;
+
+	ButtonImageEx() {
+		setTitle("버튼에 아이콘 달기 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane = getContentPane();
+		contentPane.setLayout(new FlowLayout());
+
+		ImageIcon normalIcon = new ImageIcon("images/normalIcon.gif");
+		ImageIcon rolloverIcon = new ImageIcon("images/rolloverIcon.gif");
+		ImageIcon pressedIcon = new ImageIcon("images/pressedIcon.gif");
+		JButton btn = new JButton("call~~", normalIcon);
+		btn.setRolloverIcon(rolloverIcon);
+		btn.setPressedIcon(pressedIcon);
+		contentPane.add(btn);
+
+		setSize(250, 200);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new ButtonImageEx();
+	}
+}
