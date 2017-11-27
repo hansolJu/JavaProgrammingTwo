ce3f76f3ce744df0d76c2e9d3d2630db314bccdf
컴퓨터 <@DESKTOP-C01FUL7>
2017-11-27 10:49:32 +0900

    예제 13-6 wait(), notify()를  이용한 바 채우기

---
+++ JavaProgrammingTwo/src/Chapther13_6/ConsumerThread.java
@@ -0,0 +1,18 @@
+package Chapther13_6;
+
+public class ConsumerThread extends Thread {
+	MyLabel bar;
+
+	ConsumerThread(MyLabel bar) {
+		this.bar = bar;
+	}
+
+	public void run() {
+		while (true) {
+			try {
+				sleep(200);
+				bar.consume();
+			} catch (InterruptedException e) {return;}
+		}
+	}
+}
---
+++ JavaProgrammingTwo/src/Chapther13_6/MyLabel.java
@@ -0,0 +1,50 @@
+package Chapther13_6;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JLabel;
+
+class MyLabel extends JLabel {
+	int barSize = 0; // 바의크기
+	int maxBarSize;
+
+	MyLabel(int maxBarSize) {
+		this.maxBarSize = maxBarSize;
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.MAGENTA);
+		int width = (int) (((double) (this.getWidth())) / maxBarSize * barSize);
+		if (width == 0)
+			return;
+		g.fillRect(0, 0, width, this.getHeight());
+	}
+
+	synchronized void fill() {
+		if (barSize == maxBarSize) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				return;
+			}
+		}
+		barSize++;
+		repaint(); // 바 다시 그리기
+		notify();
+	}
+
+	synchronized void consume() {
+		if (barSize == 0) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				return;
+			}
+		}
+		barSize--;
+		repaint(); // 바 다시 그리기
+		notify();
+	}
+}
